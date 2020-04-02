a278aa17f78a3ccb03f64297f3ad52195e9c19f9
Thomas <thomas.guillaume@efrei.net>
2020-04-02 16:41:02 +0200

    init files

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,36 @@
 package application;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.awt.*;
+import java.sql.*;
 
-    public static void main(String[] args) {
-        launch(args);
+import database.OracleDB;
+
+public class Main {
+
+    public static void main(String[] args){
+        //launch(args);
+
+        OracleDB database = new OracleDB();
+
+        database.connectionDatabase("thomas", "oracle");
+        database.closeDatabase();
+
     }
 
-    @Override
+
+    /*@Override
     public void start(Stage primaryStage) {
-
-    }
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        StackPane root = new StackPane();
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }*/
 }
--- src/database/OracleDB.java
+++ src/database/OracleDB.java
@@ -1,4 +1,58 @@
 package database;
 
+import java.sql.*;
+
+
+/**
+ *  Database class
+ */
 public class OracleDB {
+
+    private Connection connection;
+
+    // Constructor
+
+    public OracleDB(){}
+
+    // Connexion methods
+
+    public void connectionDatabase(String username, String password){
+        try {
+            // Connection to oracle database
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            this.connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:51521:xe", username, password);
+
+            System.out.println(" *** Connection successfully established ***");
+            System.out.println("Welcome " + username);
+
+
+        }catch(SQLException ex){
+            System.out.println(" *** ERROR CONNECTION TO DATABASE ***");
+            while (ex != null) {
+                System.out.println ("SQL Etat: " + ex.getSQLState ());
+                System.out.println ("Message: " + ex.getMessage ());
+                System.out.println ("Error code: " +
+                        ex.getErrorCode ());
+                ex = ex.getNextException ();
+            }
+        }catch(ClassNotFoundException ex){
+            System.out.println(" *** ERROR LOADING DRIVER *** ");
+            while (ex != null) {
+                System.out.println("Message: " + ex.getMessage());
+                System.out.println("Error code: " + ex.getCause());
+            }
+        }
+    }
+
+    public void closeDatabase(){
+        if(connection != null) {
+            try {
+                connection.close();
+                System.out.println(" *** Connection close ***");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
