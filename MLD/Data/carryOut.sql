e85ff77f5b2c56a750eb2174c0b96dcce0ec9084
Thomas <thomas.guillaume@efrei.net>
2020-04-09 21:22:25 +0200

    commit

--- MLD/Data/carryOut.sql
+++ MLD/Data/carryOut.sql
@@ -1,12 +1,17 @@
-insert into consultation_carryout (consultation_id, patient_id) values (1, 4);
-insert into consultation_carryout (consultation_id, patient_id) values (2, 7);
-insert into consultation_carryout (consultation_id, patient_id) values (3, 3);
-insert into consultation_carryout (consultation_id, patient_id) values (4, 5);
-insert into consultation_carryout (consultation_id, patient_id) values (5, 3);
-insert into consultation_carryout (consultation_id, patient_id) values (6, 9);
-insert into consultation_carryout (consultation_id, patient_id) values (7, 4);
-insert into consultation_carryout (consultation_id, patient_id) values (8, 6);
-insert into consultation_carryout (consultation_id, patient_id) values (9, 9);
-insert into consultation_carryout (consultation_id, patient_id) values (10, 2);
+insert into consultation_carryOut (patient_id, consultation_id) values (2, 8);
+insert into consultation_carryOut (patient_id, consultation_id) values (1, 6);
+insert into consultation_carryOut (patient_id, consultation_id) values (1, 3);
+insert into consultation_carryOut (patient_id, consultation_id) values (5, 8);
+insert into consultation_carryOut (patient_id, consultation_id) values (3, 8);
+insert into consultation_carryOut (patient_id, consultation_id) values (4, 9);
+insert into consultation_carryOut (patient_id, consultation_id) values (2, 7);
+insert into consultation_carryOut (patient_id, consultation_id) values (4, 4);
+insert into consultation_carryOut (patient_id, consultation_id) values (5, 6);
+insert into consultation_carryOut (patient_id, consultation_id) values (2, 2);
+insert into consultation_carryOut (patient_id, consultation_id) values (1, 7);
+insert into consultation_carryOut (patient_id, consultation_id) values (5, 3);
+insert into consultation_carryOut (patient_id, consultation_id) values (1, 1);
+insert into consultation_carryOut (patient_id, consultation_id) values (1, 2);
+insert into consultation_carryOut (patient_id, consultation_id) values (1, 5);
 
 commit;
